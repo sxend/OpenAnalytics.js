3e1f28a615e7c499ebba64f93f8ca0320d3695fa
sxend <arimitsu.k+github@gmail.com>
2020-11-19 21:24:19 +0900

    consume old queue

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,3 +1,11 @@
-export function factory(_OpenAnalyticsObject: string) {
-  return function analytics() {};
+export function factory(oa: any) {
+  function analytics(...params: any[]) {
+    console.log(...params);
+  }
+  if (oa.q) {
+    for (const command of oa.q) {
+      analytics(...command);
+    }
+  }
+  return analytics;
 }
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { factory } from "./analytics";
 
 (function (global) {
   const OpenAnalyticsObject = global.OpenAnalyticsObject || "oa";
-  global[OpenAnalyticsObject] = factory(OpenAnalyticsObject);
+  global[OpenAnalyticsObject] = factory(global[OpenAnalyticsObject]);
 })((0, eval)("this"));
