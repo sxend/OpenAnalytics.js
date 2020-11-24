359a157236036dc9eaaef773be0084a96630b1b2
sxend <arimitsu.k+github@gmail.com>
2020-11-24 21:40:38 +0900

    add create method

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,26 +1,36 @@
+export interface OpenAnalytics {
+  q?: any[][];
+  t: { [name: string]: Tracker };
+  L: number;
+  (...params: any): void;
+  (fn: (oa?: OpenAnalytics) => void): void;
+}
+
 const DEFAULT_TRACKER_NAME = "t0";
 const methods: { [methodName: string]: Function } = {
-  create: create,
+  create,
 };
 
-export function factory(oa: any) {
+export function factory(oa: OpenAnalytics) {
   if (!oa?.q) return oa;
-  function analytics(...params: any[]) {
-    const headParam = params[0];
-    if (typeof headParam === "function") {
-      // https://developers.google.com/analytics/devguides/collection/analyticsjs/command-queue-reference?hl=en#ready-callback
-      headParam(analytics.t[DEFAULT_TRACKER_NAME]);
-      return;
-    } else if (typeof headParam === "string") {
-      const command = parseCommand(headParam);
-      if (!command?.methodName) return;
-      if (!methods[command.methodName]) return;
-      methods[command.methodName](analytics, params);
-      console.log(command);
+  const analytics: OpenAnalytics = (() => {
+    function analytics(...params: any[]) {
+      const headParam = params[0];
+      if (typeof headParam === "function") {
+        // https://developers.google.com/analytics/devguides/collection/analyticsjs/command-queue-reference?hl=en#ready-callback
+        headParam(analytics.t[DEFAULT_TRACKER_NAME]);
+        return;
+      } else if (typeof headParam === "string") {
+        const command = parseCommand(headParam);
+        if (!command?.methodName) return;
+        if (!methods[command.methodName]) return;
+        methods[command.methodName]?.(analytics, ...params.slice(1));
+      }
     }
-  }
-  analytics.t = <any>{}; // trackers
-  analytics.L = Date.now(); // load time
+    analytics.t = <{ [name: string]: Tracker }>{}; // trackers
+    analytics.L = Date.now(); // load time
+    return analytics;
+  })();
   for (const command of oa?.q || []) {
     analytics(...command);
   }
@@ -29,16 +39,29 @@ export function factory(oa: any) {
 const COMMAND_REGEX = /^(\w+\.)?(\w+\:)?(\w+)$/;
 function parseCommand(command: string) {
   // https://developers.google.com/analytics/devguides/collection/analyticsjs/command-queue-reference?hl=en#parameters
-  const [ok, trackerName, pluginName, methodName] =
+  const [, trackerName, pluginName, methodName] =
     COMMAND_REGEX.exec(command) || [];
-  if (!ok) return <any>{};
   return {
     trackerName: trackerName?.replace(".", ""),
     pluginName: pluginName?.replace(":", ""),
     methodName,
   };
 }
+class Tracker {
+  constructor(public model: Model) {}
+}
+class Model {}
 
-function create(_oa: any, ...params: any[]) {
-  console.log(params);
+function create(
+  oa: OpenAnalytics,
+  trackingId?: string,
+  cookieDomain?: string,
+  name?: string,
+  fieldsObject: any = {}
+) {
+  if (oa.t[name]) return;
+  fieldsObject.trackingId = trackingId;
+  fieldsObject.cookieDomain = cookieDomain;
+  const model = new Model();
+  oa.t[name] = new Tracker(model);
 }
